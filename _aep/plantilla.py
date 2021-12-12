4e68edc82d089a34d167616ab77c0538276bee84
leugimkm <leugim911@hotmail.com>
2021-12-11 22:19:02 -0500

    added template

---
+++ _aep/plantilla.py
@@ -0,0 +1,29 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+# ------------------------- Enunciado Original ------------------------
+Aqui va el enunciado del problema
+# ------------------------ Enunciado Modificado -----------------------
+Si tuviera que ser modificado el enunciado, lo haría aquí.
+# ---------------------------------------------------------------------
+NOTE: Notas sobrel el enunciado del problema.
+"""
+from unittest import main, TestCase
+
+
+def solver():
+    ...
+
+
+def main_():
+    ...
+
+
+class Test(TestCase):
+
+    def test_solver(self):
+        ...
+
+
+if __name__ == "__main__":
+    # main() # uncomment this line and comment the next one to run tests
+    main_()
